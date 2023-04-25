7d5cfb0ad42c4702cf6591801515b340b3d315fb
Zahra-Whitehat <zahradorostkar@yahoo.com>
2023-04-25 19:33:13 +0300

    SHit PyCharM, fucking PusH

---
+++ Codebook.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# Define the generator matrix
+G = np.array([
+    [1, 0, 0, 0, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1],
+    [0, 0, 1, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1]
+])
+
+# Generate all possible message words
+messages = np.array([
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 1, 0, 0],
+    [0, 1, 0, 1],
+    [0, 1, 1, 0],
+    [0, 1, 1, 1],
+    [1, 0, 0, 0],
+    [1, 0, 0, 1],
+    [1, 0, 1, 0],
+    [1, 0, 1, 1],
+    [1, 1, 0, 0],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0],
+    [1, 1, 1, 1]
+])
+
+# Encode the messages to obtain the code words
+codebook = messages @ G % 2
+
+# Print the codebook
+print(codebook)
+print("")
